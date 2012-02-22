7ef374a3230f0f23c91613614f4be4f1bf43820d
Hamish King <heymishy@gmail.com>
2012-02-22 22:16:20 +1100

    added template and URL.confs to understand how django handles variables and templates - next step to retrieve from MongoDB

--- urls.py
+++ urls.py
@@ -1,12 +1,16 @@
 # This also imports the include function
 from django.conf.urls.defaults import *
-
+from words.views import *
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/$', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^words/', include('words.urls')),
+#   url(r'^words/', include('words.urls')),
+    url(r'^words/$', 'words.views.index'),
+    url(r'^words/time$', 'words.views.current_datetime'),
+    url(r'^words/db$', 'words.views.get_string'),
+
 )
 
--- words/views.py
+++ words/views.py
@@ -1,12 +1,37 @@
 # Create your views here.
-import nltk
-from words.models import Document, Sents, Words
-from mimetypes import guess_type
-from gridfs.errors import NoFile
+#import nltk
+from words.models import Document, Sentences, Words
+# mimetypes import guess_type
+#from gridfs.errors import NoFile
 from django.http import HttpResponse, Http404
 from django.conf import settings
-from words import gridfs_storage
-from words.models import FileUpload
+#from words import gridfs_storage
+import datetime
+# from words.models import FileUpload
+from django.template import Context, loader
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def index(request):
+    t = loader.get_template('words/index.html')
+    c = Context({
+        'string': 'string'
+    })
+    return HttpResponse(t.render(c))
+
+def get_string(request):
+  #  d = Document    
+  #  define code to retrieve a document and hold in variable 
+   t = loader.get_template('words/index.html')
+   c = Context({
+       'get_string': 'string'
+   })
+   return HttpResponse(t.render(c))
+
+''' NLTK tokenize code - removed for now
 
 f = open("/home/hamish/Documents/test.txt")
 raw = f.read()
@@ -14,12 +39,12 @@ tokens = nltk.word_tokenize(raw)
 sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle')
 sents = sent_tokenizer.tokenize(raw)
 
-# d = Document(title="Manual test entry", creation_date=datetime.datetime.now(), author="Hamish",comments="NA")
-# Document.objects.get()._id
+ d = Document(title="Manual test entry", creation_date=datetime.datetime.now(), author="Hamish",comments="NA")
+ Document.objects.get()._id
 for sents in sents:
     s = Sents(body=sents)
     s.save()
-
+'''
 # Grid FS File Upload in Admin code
 
 '''if settings.DEBUG:
